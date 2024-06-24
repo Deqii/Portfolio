c101256e3d8bc928e600ec56708296db9a0aec35
tioprayudha <tioprayudha07@gmail.com>
2024-06-24 09:05:03 +0700

    Header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,23 @@
+// links
+import Link from "next/link";
+
+// icons
+import { RiYoutubeLine, RiInstagramLine, RiFacebookLine } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent trnasition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={""} className="hover:text-accent trnasition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent trnasition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
